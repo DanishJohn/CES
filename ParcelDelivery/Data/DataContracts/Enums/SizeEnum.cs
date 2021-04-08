9cab2c3531c0c06a635407725d66556bc04dd222
mav <mav@PF23ZX1D>
2021-04-09 01:05:39 +0700

    - Add SizeEnum

---
+++ ParcelDelivery/Data/DataContracts/Enums/SizeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcelDelivery.Data.DataContracts.Enums
+{
+    public enum SizeEnum
+    {
+        LessThan25,
+        Between25And40,
+        MoreThan40,
+    }
+}
